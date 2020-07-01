683577bcd2c0d3135da5e3590a6571cfed2ceeb3
lamiyaaboobakkar <66775164+lamiyaaboobakkar@users.noreply.github.com>
2020-07-01 13:51:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', function(req, res){
-  res.send('Hello from Docker World' );
+  res.send('Hello from Docker Coomunity' );
 
 
 
